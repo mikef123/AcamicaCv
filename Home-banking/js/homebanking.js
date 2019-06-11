efdaa6cf1fc9704c1cf5e16bc746372ae65d15b9
Miguel Fernando Urquijo Gómez <miguelferu@gmail.com>
2019-06-10 21:47:50 -0500

    Ajuste pantalla eventos

--- Home-banking/js/homebanking.js
+++ Home-banking/js/homebanking.js
@@ -1,7 +1,10 @@
 //Declaración de variables
+let nombreUsuario = 'mike';
+let saldoCuenta = 10000;
+let limiteExtraccion = 8000;
 
 //Ejecución de las funciones que actualizan los valores de las variables en el HTML.
-window.onload = function() {
+window.onload = function () {
     cargarNombreEnPantalla();
     actualizarSaldoEnPantalla();
     actualizarLimiteEnPantalla();
@@ -9,15 +12,37 @@ window.onload = function() {
 
 
 //Funciones que tenes que completar
+function CalcularTransaccion(accion) {
+    swal("Digita la cantidad de dinero a depositar:", {
+        content: "input",
+    })
+        .then((value) => {
+            var saldoActual = saldoCuenta;
+            saldoCuenta += parseInt(value);
+            swal("Genial!", `Has depositado: $${value} 
+             Saldo anterior: $${saldoActual} 
+             Saldo actual: $${saldoCuenta}`);
+            actualizarSaldoEnPantalla();
+        });
+}
+
+
 function cambiarLimiteDeExtraccion() {
 
 }
 
-function extraerDinero() {
-
+function extraerDinero(dineroARetirar) {
+    if (dineroARetirar < saldoCuenta) {
+        saldoCuenta -= dineroARetirar;
+    }
+    else {
+        swal("Upss", "La cantidad a retirar es mayor a el saldo en la cuenta", "error");
+    }
 }
 
 function depositarDinero() {
+
+    CalcularTransaccion('Depositar');
 
 }
 
