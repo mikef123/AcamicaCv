8da22cf848a0a77df15c4d19687741eb70176338
Miguel Fernando Urquijo Gómez <miguelferu@gmail.com>
2019-06-27 23:32:55 -0500

    Proyecto pixeles

--- Recursos/js/pixel-art.js
+++ Recursos/js/pixel-art.js
@@ -23,13 +23,86 @@ var nombreColores = ['White', 'LightYellow',
 // Variable para guardar el elemento 'color-personalizado'
 // Es decir, el que se elige con la rueda de color.
 var colorPersonalizado = document.getElementById('color-personalizado');
+var paleta = document.getElementById('paleta');
+var grilla = document.getElementById('grilla-pixeles');
+var seleccion = document.getElementById('indicador-de-color');
+var borrar = document.getElementById('borrar');
+var imagenes = document.getElementsByClassName('imgs');
 
+// Variables
+var seleccionado = false;
+
+
+/// eventos
 colorPersonalizado.addEventListener('change', 
   (function() {
+    debugger
     // Se guarda el color de la rueda en colorActual
     colorActual = colorPersonalizado.value;
     // Completar para que cambie el indicador-de-color al colorActual
+    seleccion.style.backgroundColor = colorActual;
 
 
   })
 );
+
+paleta.addEventListener('click', modificarColor);
+ grilla.addEventListener('mousedown', () => {
+  seleccionado = true;
+ });
+grilla.addEventListener('mouseover', rellenarGrilla);
+grilla.addEventListener('mouseup', ()=> {
+  seleccionado = false;
+});
+
+// recorre los colores y rellena la paleta de colores.
+function recorrerColores()
+{
+  for (let index = 0; index < nombreColores.length; index++) {
+    const element = nombreColores[index];
+    var div = document.createElement('div');
+    div.className = "color-paleta";
+    div.style.backgroundColor = element;
+    paleta.appendChild(div);
+  }
+}
+
+// Llena la grilla con los pixeles.
+function llenarGrilla () {
+  for (let index = 0; index < 1750; index++) {
+    const div = document.createElement('div');
+    grilla.appendChild(div);
+  }
+}
+
+//Llamamiento de la función.
+recorrerColores();
+llenarGrilla ();
+
+// Modifica el color seleccionado.
+function modificarColor (e) {
+  seleccion.style.backgroundColor = e.target.style.backgroundColor;
+}
+
+// Modifica el color seleccionado en la grilla.
+function rellenarGrilla (e) {
+  if(seleccionado) {
+    var seleccion = document.getElementById('indicador-de-color');
+    e.target.style.backgroundColor = seleccion.style.backgroundColor;
+  }
+}
+
+//Realizar el borrado con animación
+borrar.addEventListener('click', () => {
+  $(grilla).children('div').animate({"backgroundColor":"white"}, 2000);
+});
+
+// Seleccionar imagen a cargar
+imagenes.addEventListener('click', seleccionarImagen);
+
+
+
+function seleccionarImagen (e) {
+  debugger
+}
+
