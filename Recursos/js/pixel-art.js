46024d336b2c111b77f7f4ae5b254f27584b5909
Miguel Fernando Urquijo Gómez <miguelferu@gmail.com>
2019-06-28 15:03:12 -0500

    Proyecto terminado Pixel Art

--- Recursos/js/pixel-art.js
+++ Recursos/js/pixel-art.js
@@ -27,33 +27,32 @@ var paleta = document.getElementById('paleta');
 var grilla = document.getElementById('grilla-pixeles');
 var seleccion = document.getElementById('indicador-de-color');
 var borrar = document.getElementById('borrar');
-var imagenes = document.getElementsByClassName('imgs');
+var borrarPixel = document.getElementById('borrarPixel');
+var guardar = document.getElementById("guardar");
+var copiarColor = document.getElementById("copiarColor");
 
 // Variables
 var seleccionado = false;
+var estadoCopiaColor = false;
 
 
 /// eventos
-colorPersonalizado.addEventListener('change', 
-  (function() {
-    debugger
+paleta.addEventListener('click', modificarColor);
+grilla.addEventListener('click', llenarDiv);
+grilla.addEventListener('mousedown', () => { seleccionado = true; });
+grilla.addEventListener('mouseover', rellenarGrilla);
+grilla.addEventListener('mouseup', () => { seleccionado = false; });
+colorPersonalizado.addEventListener('change',
+  (function () {
     // Se guarda el color de la rueda en colorActual
     colorActual = colorPersonalizado.value;
     // Completar para que cambie el indicador-de-color al colorActual
     seleccion.style.backgroundColor = colorActual;
-
-
   })
 );
 
-paleta.addEventListener('click', modificarColor);
- grilla.addEventListener('mousedown', () => {  seleccionado = true; });
-grilla.addEventListener('mouseover', rellenarGrilla);
-grilla.addEventListener('mouseup', ()=> {  seleccionado = false;});
-
 // recorre los colores y rellena la paleta de colores.
-function recorrerColores()
-{
+function recorrerColores() {
   for (let index = 0; index < nombreColores.length; index++) {
     const element = nombreColores[index];
     var div = document.createElement('div');
@@ -64,7 +63,7 @@ function recorrerColores()
 }
 
 // Llena la grilla con los pixeles.
-function llenarGrilla () {
+function llenarGrilla() {
   for (let index = 0; index < 1750; index++) {
     const div = document.createElement('div');
     grilla.appendChild(div);
@@ -73,32 +72,75 @@ function llenarGrilla () {
 
 //Llamamiento de la función.
 recorrerColores();
-llenarGrilla ();
+llenarGrilla();
 
 // Modifica el color seleccionado.
-function modificarColor (e) {
-  seleccion.style.backgroundColor = e.target.style.backgroundColor;
+function modificarColor(e) {
+    seleccion.style.backgroundColor = e.target.style.backgroundColor;
 }
 
 // Modifica el color seleccionado en la grilla.
-function rellenarGrilla (e) {
-  if(seleccionado) {
-    var seleccion = document.getElementById('indicador-de-color');
-    e.target.style.backgroundColor = seleccion.style.backgroundColor;
+function rellenarGrilla(e) {
+  if (seleccionado) {
+    llenarDiv(e);
   }
 }
 
 //Realizar el borrado con animación
 borrar.addEventListener('click', () => {
-  $(grilla).children('div').animate({"backgroundColor":"white"}, 2000);
+  $(grilla).children('div').animate({ "backgroundColor": "white" }, 2000);
 });
 
+
 // Seleccionar imagen a cargar
-imagenes.addEventListener('click', seleccionarImagen);
+$(".imgs li").on("click", function (e) {
 
+  var sh = e.target.id;
+  switch (sh) {
+    case 'batman':
+      cargarSuperheroe(batman);
+      break;
+    case 'invisible':
+      cargarSuperheroe(invisible);
+      break;
+    case 'flash':
+      cargarSuperheroe(flash);
+      break;
+    case 'wonder':
+      cargarSuperheroe(wonder);
+      break;
+    default:
+      console.log("Error")
+      break;
+  }
 
+})
 
-function seleccionarImagen (e) {
-  debugger
+//Llena o copia el color del background.
+function llenarDiv(e) {
+  var seleccion = document.getElementById('indicador-de-color');
+  if (estadoCopiaColor) {
+    seleccion.style.backgroundColor = e.target.style.backgroundColor;
+  } else {
+    e.target.style.backgroundColor = seleccion.style.backgroundColor;
+  }
 }
 
+//función que permite guardar en formato png.
+guardar.addEventListener("click", guardarPixelArt);
+
+//Permite borrar pixeles y dejarlos en blanco.
+borrarPixel.addEventListener("click", () => {
+  seleccion.style.backgroundColor = "white";
+});
+
+//Permite copiar un color para poder replicarlo en otro pixel.
+copiarColor.addEventListener('click', () => {
+  if (estadoCopiaColor) {
+    $("#copiarColor").css("backgroundColor", "lightgrey");
+    estadoCopiaColor = false;
+  } else {
+    $("#copiarColor").css("backgroundColor", "red");
+    estadoCopiaColor = true;
+  }
+});
